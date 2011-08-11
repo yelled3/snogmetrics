4c6edf723eaf6ecdd9040c9b94803288c1660f1a
Adam Farhi <adam@giftsproject.com>
2011-08-11 05:39:40 -0600

    Another fix, removed duplicated js tags

--- lib/snogmetrics.rb
+++ lib/snogmetrics.rb
@@ -164,17 +164,15 @@ private
         JS
       elsif @output_strategy == :live
         <<-END
-          <script type="text/javascript">
-            var _kmq = _kmq || [];
-            function _kms(u){
-              setTimeout(function(){
-                var s = document.createElement('script'); var f = document.getElementsByTagName('script')[0];
-                s.type = 'text/javascript'; s.async = true;
-                s.src = u; f.parentNode.insertBefore(s, f);
-              }, 1);
-            }
-            _kms('//i.kissmetrics.com/i.js');_kms('//doug1izaerwt3.cloudfront.net/#{@api_key}.1.js');
-          </script>
+          var _kmq = _kmq || [];
+          function _kms(u){
+            setTimeout(function(){
+              var s = document.createElement('script'); var f = document.getElementsByTagName('script')[0];
+              s.type = 'text/javascript'; s.async = true;
+              s.src = u; f.parentNode.insertBefore(s, f);
+            }, 1);
+          }
+          _kms('//i.kissmetrics.com/i.js');_kms('//doug1izaerwt3.cloudfront.net/#{@api_key}.1.js');
         END
       elsif @output_strategy == :array
         ""
